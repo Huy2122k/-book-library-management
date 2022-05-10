1fbb7fecbeb8b5d1e03c549bdd758a6bd8b79b85
Huy2122k <wwwhuy212@gmail.com>
2022-05-10 15:44:42 +0700

    add Login & Register (UI)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,16 +4,31 @@ import './App.css';
 
 import Home from './components/Home';
 
+import RegistrationForm from './components/register/RegisterUser';
 
 import ProvideAuth from './auth/ProvideAuth';
+import RequireAuth from './auth/RequireAuth';
+import BoardAdmin from './components/BoardAdmin';
+import BoardUser from './components/BoardUser';
 import LayoutCustom from './components/layout';
+import Login from './components/login/Login';
+import Profile from './components/Profile';
 const App = () => {
     return (
         <ProvideAuth>
             <Routes>
                 <Route element={<LayoutCustom />}>
                     <Route path="/" element={<Home />} />
-                   
+                    <Route path="/home" element={<Home />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<RegistrationForm />} />
+                    <Route element={<RequireAuth role={['USER']} />}>
+                        <Route path="/profile" element={<Profile />} />
+                        <Route path="/user" element={<BoardUser />} />
+                    </Route>
+                    <Route element={<RequireAuth role={['ADMIN']} />}>
+                        <Route path="/admin" element={<BoardAdmin />} />
+                    </Route>
                 </Route>
                 <Route path="*" element={<h3>NotFound</h3>} />
             </Routes>
