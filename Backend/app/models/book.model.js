afd70f78168d73ff26b977b98b910996787c8827
Huy2122k <wwwhuy212@gmail.com>
2022-05-10 15:21:38 +0700

    init Backend

---
+++ Backend/app/models/book.model.js
@@ -0,0 +1,30 @@
+module.exports = (sequelize, Sequelize) => {
+    const Book = sequelize.define("book", {
+        BookID: {
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4,
+            primaryKey: true,
+        },
+        BookName: {
+            type: Sequelize.TEXT,
+        },
+        Author: {
+            type: Sequelize.STRING,
+        },
+        Description: {
+            type: Sequelize.TEXT,
+        },
+        CategoryID: {
+            type: Sequelize.INTEGER,
+            references: {
+                model: "category",
+                key: "CategoryID",
+            },
+        },
+        ImageURL: {
+            type: Sequelize.STRING,
+        },
+    });
+
+    return Book;
+};
